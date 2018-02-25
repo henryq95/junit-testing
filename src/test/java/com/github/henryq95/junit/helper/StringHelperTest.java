fdece0c427dc9ed246bf3c3db264cfe6d21b4c6c
henryq95 <quinteros7766@gmail.com>
2018-02-25 13:56:41 -0800

    added test cases to truncateAInFirst2Positions

--- src/test/java/com/github/henryq95/junit/helper/StringHelperTest.java
+++ src/test/java/com/github/henryq95/junit/helper/StringHelperTest.java
@@ -6,12 +6,25 @@ import static org.junit.jupiter.api.Assertions.*;
 //JUnit 5
 
 class StringHelperTest {
+    @Test
+    public void test(){
+        assertEquals("ABC", "ABC");
+        //expected, actual
+    }
+
 
     @Test
-    void truncateAInFirst2Positions() {
+    public void testTruncateAInFirst2Positions_Cond() {
+        StringHelper helper = new StringHelper();
+        //AACD=>CD
+        assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
+        assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+        //not good to have two test conditions in the same test
+        //expected, actual
     }
 
     @Test
-    void areFirstAndLastTwoCharactersTheSame() {
+    public void testAreFirstAndLastTwoCharactersTheSame_Cond() {
+
     }
 }
