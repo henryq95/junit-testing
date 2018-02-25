5c4cde8c6585ed74d14a1d47eaaaafa14fdb81da
henryq95 <quinteros7766@gmail.com>
2018-02-25 13:41:06 -0800

    Added StringHelper class and empty StringHelperTest class

---
+++ src/test/java/com/github/henryq95/junit/helper/StringHelperTest.java
@@ -0,0 +1,17 @@
+package com.github.henryq95.junit.helper;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+//JUnit 5
+
+class StringHelperTest {
+
+    @Test
+    void truncateAInFirst2Positions() {
+    }
+
+    @Test
+    void areFirstAndLastTwoCharactersTheSame() {
+    }
+}
