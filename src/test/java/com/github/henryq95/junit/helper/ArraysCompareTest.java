20857f4da5d46054f35413cf1e44c884c26db4ec
henryq95 <quinteros7766@gmail.com>
2018-02-27 15:43:46 -0800

    added a performance test via assertTimeout on sorting an array

--- src/test/java/com/github/henryq95/junit/helper/ArraysCompareTest.java
+++ src/test/java/com/github/henryq95/junit/helper/ArraysCompareTest.java
@@ -4,22 +4,34 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
+import static java.time.Duration.ofSeconds;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ArraysCompareTest {
     @Test
     public void testArraysSort_RandomArray() {
-        int[] numbers = {12,3,4,1};
-        int[] expected = {1,3,4,12};
+        int[] numbers = {12, 3, 4, 1};
+        int[] expected = {1, 3, 4, 12};
         Arrays.sort(numbers);
         assertArrayEquals(expected, numbers);
     }
 
     @Test
     public void testArraySort_NullArray() {
-        assertThrows(NullPointerException.class, ()->{
+        assertThrows(NullPointerException.class, () -> {
             int[] numbers = null;
             Arrays.sort(numbers);
+        });
+    }
+
+    @Test
+    public void testSort_Performance() {
+        assertTimeout(ofSeconds(1), () -> {
+            int array[] = {12, 23, 4};
+            for (int i = 1; i <= 1000000; i++) {
+                array[0] = i;
+                Arrays.sort(array);
+            }
         });
     }
 }
